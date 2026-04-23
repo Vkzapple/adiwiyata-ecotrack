aa1786682063ece0cc56547752ce8f4bcdfdcd5c
evell <evellykhnz@gmail.com>
2026-04-23 10:19:21 +0700

    update camera

--- website/next.config.ts
+++ website/next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
-  experimental: {
-    reactCompiler: false,
-  },
+const nextConfig: NextConfig = {
+  reactCompiler: false,
+  allowedDevOrigins: ['10.230.6.253'],
 };
 
 export default nextConfig;
